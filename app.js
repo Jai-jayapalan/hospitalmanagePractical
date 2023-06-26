a066ee56166b8afaa76a727616501436295c27c6
Jai_Jayathilak <jaijayathilak@gmail.com>
2023-06-26 09:11:23 +0530

    routes are added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express()
-
+const initialData = require('./Data/initialData')
+const hospitalModel = require('./Model/hospitalModel')
 
 // middleware
 app.use(cors())
@@ -17,6 +18,52 @@ db.on('error',(err)=>{
 })
 db.once('open',()=>{
     console.log('Connected to database successfully.....')
+})
+
+// getAllPatients
+app.get('/api/v1/patients',async(req,res)=>{
+    try{
+        const patientData = await hospitalModel.find()
+
+        // if the data is not present in the db then add the predefined data
+        if(patientData.lenght===0){
+            patientData = await hospitalModel.insertMany(initialData)
+        }
+        res.status(201).json(patientData)
+    }
+    catch (Err){
+        res.status(500).json({Error: Err.message})
+    }
+})
+
+// post patient data
+app.post('/api/v1/patients',async(req,res)=>{
+    const newPatient = req.body;
+    try{
+        // finding existing patient
+        const existingPatient = await hospitalModel.findOne({patientID:request.body.patientID})
+        if(existingPatient){
+            res.json({error:"User already exits"})
+        }else{
+            const addnewPatient = await hospitalModel.create(newPatient)
+            res.status(201).json(addnewPatient)
+        }
+    }
+    catch (error){
+        res.status(500).json({error:error.message})
+    }
+})
+
+// update patientData
+app.put('/api/v1/patients', async(req,res)=>{
+    const patientToBeUpdated = req.body;
+    try{
+        const patient = await hospitalModel.findByIdAndUpdate({patientID: patientToBeUpdated.patientID},patientToBeUpdated)
+        res.status(201).json(patient)
+    }
+    catch (error){
+        res.status(500).json({Error: error.message})
+    }
 })
 
 
